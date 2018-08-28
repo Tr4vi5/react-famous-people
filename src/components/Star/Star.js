4ea70a8ed617d47b07dbdd18cbb07b5beaf30738
Tr4vi5 <trav.dunn@outlook.com>
2018-08-28 15:17:08 -0500

    stretch goals

--- src/components/Star/Star.js
+++ src/components/Star/Star.js
@@ -3,30 +3,34 @@ import React, { Component } from 'react';
 class Star extends Component {
     constructor(){
         super();
-        this.state = { name: '', role: '' };
+        this.state = { star: { name: '', role: ''} };
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleRoleChange = this.handleRoleChange.bind(this);
         this.logName = this.logName.bind(this);
     }
 
-
     handleNameChange(event){
-        this.setState({name: event.target.value});
+        let role = this.state.star.role;
+        this.setState({
+            star: {name: event.target.value, role}
+        });
     }
 
     handleRoleChange(event){
-        this.setState({role: event.target.value});
+        let name = this.state.star.name;
+        this.setState({
+            star: {name, role: event.target.value}
+        });
     }
 
     logName(){
-        console.log('Star\'s name is ', this.state.name);
-        
+        console.log('Star\'s name is ', this.state.star);
     }
 
     render(){
         return(
             <div>
-            <p>{this.state.name} is famous for {this.state.role}.</p>
+            <p>{this.state.star.name} is famous for {this.state.star.role}.</p>
             <input onChange={this.handleNameChange} />
             <input onChange={this.handleRoleChange} />
             <button onClick={this.logName}>Console Log Please</button>
